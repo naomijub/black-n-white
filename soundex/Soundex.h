16c564ccfd99a54cda3c2359d56416696af826b7
Julia Naomi <jnboeira@outlook.com>
2021-04-21 17:50:17 -0300

    functional like C++

--- soundex/Soundex.h
+++ soundex/Soundex.h
@@ -3,6 +3,7 @@
 
 #include "string"
 #include "unordered_map"
+#include "algorithm"
 
 static const size_t MAX_CODE_LENGTH{4};
 
@@ -26,22 +27,25 @@ class Soundex {
     std::string encodeDigits(const std::string& word) const {
       if (word.empty()) return "";
       std::string enconding;
-      for (auto letter: word) enconding += encodeDigit(letter);
+      enconding.resize(word.length());
+      std::transform(word.begin(), word.end(), enconding.begin(), encodeDigit);
+
       return enconding;
     }
 
-    std::string encodeDigit(char letter) const {
-      const std::unordered_map<char, std::string> encondings {
-        {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
-        {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"}, 
-          {'s', "2"}, {'x', "2"}, {'z', "2"}, 
-        {'d', "3"}, 
-        {'l', "4"},
-        {'m', "5"}, {'n', "5"}, 
-        {'r', "6"},
+    static char encodeDigit(char letter) {
+      const std::unordered_map<char, char> encondings {
+        {'b', '1'}, {'f', '1'}, {'p', '1'}, {'v', '1'},
+        {'c', '2'}, {'g', '2'}, {'j', '2'}, {'k', '2'}, {'q', '2'}, 
+          {'s', '2'}, {'x', '2'}, {'z', '2'}, 
+        {'d', '3'}, 
+        {'l', '4'},
+        {'m', '5'}, {'n', '5'}, 
+        {'r', '6'},
       };
+      
       auto it = encondings.find(letter);
-      return it == encondings.end() ? "" : it->second;
+      return it == encondings.end() ? '0' : it->second;
     }
 
     std::string padWithZeros(const std::string& word) const {
