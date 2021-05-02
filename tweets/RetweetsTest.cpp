fcefb304ebbdd271da612c649defec870703dc08
Julia Naomi <jnboeira@outlook.com>
2021-05-02 10:32:58 -0300

    setup function

--- tweets/RetweetsTest.cpp
+++ tweets/RetweetsTest.cpp
@@ -6,7 +6,12 @@
 using namespace ::testing;
 class ARetweetCollection: public Test {
 public:
+    std::shared_ptr<Tweet> tweet;
     RetweetCollection retweets;
+
+    void SetUp() override {
+        tweet = std::shared_ptr<Tweet>(new Tweet("msg", "user"));
+    }
 };
 
 MATCHER_P(HashSize, value, "") {
@@ -22,23 +27,20 @@ TEST_F(ARetweetCollection, IsEmptyWhenCreated) {
 }
 
 TEST_F(ARetweetCollection, IsNotEmptyAfterAddingTweet) {
-    Tweet tweet("msg", "user");
-    retweets.add(tweet);
+    retweets.add(*tweet);
     ASSERT_FALSE(retweets.isEmpty());
     ASSERT_THAT(retweets.size(), Gt(0));
 }
 
 TEST_F(ARetweetCollection, HashSizeZeroAfterRemove) {
-    Tweet tweet("msg", "user");
-    retweets.add(tweet);
-    retweets.remove(tweet);
+    retweets.add(*tweet);
+    retweets.remove(*tweet);
     ASSERT_THAT(retweets, HashSize(0));
 }
 
 TEST_F(ARetweetCollection, DoesntAcceptDuplicateds) {
-    Tweet tweet("msg", "user");
-    Tweet duplicated("msg", "user");
-    retweets.add(tweet);
+    Tweet duplicated(*tweet);
+    retweets.add(*tweet);
     retweets.add(duplicated);
     ASSERT_THAT(retweets, HashSize(1));
 }
